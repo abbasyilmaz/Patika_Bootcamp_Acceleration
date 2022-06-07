05d272d0be00a17476fbb85555e474c49b1ceb6e
Abbas Yılmaz <canabbasyilmaz@gmail.com>
2022-06-07 16:05:21 +0300

    while_foreach
    while ve foeach döngülerini kullanımı

---
+++ While_foreach.cs
@@ -0,0 +1,25 @@
+﻿int i = 1;
+Random rdn = new Random();
+int rastgele = rdn.Next(0, 10);
+while (true)
+{
+    Console.Write("Sayı Giriniz:");
+    int sayi = Convert.ToInt32(Console.ReadLine());
+    if (sayi == rastgele)
+    {
+        break;
+    }
+    i++;
+}
+Console.WriteLine(i + ". tahmininizde sayıyı buldunuz.");
+
+
+
+
+string[] isimler = {"Ahmet CANSEVER","Emirhan SOYLU","Elif SÜTÇÜ",
+                                "Hakan ALTIN","Sema ÇALIŞKAN"};
+foreach (string eleman in isimler)
+{
+    Console.WriteLine(eleman);
+}
+Console.ReadKey();
